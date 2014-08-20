961f6fbc045e6af49f86fb262b84350078b980a4
Boris Konotopskyi <boris@webspellchecker.net>
2014-08-20 09:01:26 +0000

    Request #40461 : Documentation: Update development documentation for SCAYT 3 plugins
    git-svn-id: https://dev01.webspellchecker.net:8443/SVN/Editors/CKEditor/trunk/_source/plugins/scayt@6132 462193d8-cd1d-f144-b59b-5446b0a36879

--- comments.js
+++ comments.js
@@ -1,10 +1,10 @@
 /**
  * @namespace "CKEditor3 SCAYT plug-in"
  *
- * @property {Boolean} scayt_autoStartup - If enabled (set to 'true'), turns on SCAYT automatically after loading the editor.
+ * @property {Boolean} scayt_autoStartup - The parameter turns on/off SCAYT on the autostartup. If 'true', turns on SCAYT automatically after loading the editor.
  * Default value : [scayt_autoStartup=false]
  *
- * @property {Number} scayt_maxSuggestions - Defines the number of SCAYT suggestions to show in the main context menu.
+ * @property {Number} scayt_maxSuggestions - The parameter defines the number of SCAYT suggestions to show in the main context menu.
  * Possible values are:
  * '0' (zero) &ndash; No suggestions are shown in the main context menu. All
  * entries will be listed in the "More Suggestions" sub-menu.
@@ -14,12 +14,12 @@
  * entries will be listed in the "More Suggestions" sub-menu.
  * Default value : [scayt_maxSuggestions=5]
  *
- * @property {String} scayt_customerId - Sets the customer ID for SCAYT. Required for migration from free,
+ * @property {String} scayt_customerId - The parameter sets the customer ID for SCAYT. Required for migration from free,
  * ad-supported version to paid, ad-free version.
  * Default value : [scayt_customerId='1:WvF0D4-UtPqN1-43nkD4-NKvUm2-daQqk3-LmNiI-z7Ysb4-mwry24-T8YrS3-Q2tpq2'].
  * Old option name : scayt_customerid
  *
- * @property {String} scayt_moreSuggestions - Enables/disables the "More Suggestions" sub-menu in the context menu.
+ * @property {String} scayt_moreSuggestions - The parameter enables/disables the "More Suggestions" sub-menu in the context menu.
  * Possible values are 'on' and 'off'.
  * Default value : [scayt_moreSuggestions='on']
  *
@@ -32,49 +32,49 @@
  * Note, that availability of 'Options', 'Languages' and 'Dictionaries' items depends on scayt_uiTabs option also.
  * Default value : [scayt_contextCommands='ignore|ignoreall|add']
  *
- * @property {String} scayt_sLang - Sets the default spell checking language for SCAYT. Possible values are:
+ * @property {String} scayt_sLang - The parameter sets the default spell checking language for SCAYT. Possible values are:
  * 'en_US', 'en_GB', 'pt_BR', 'da_DK',
  * 'nl_NL', 'en_CA', 'fi_FI', 'fr_FR',
  * 'fr_CA', 'de_DE', 'el_GR', 'it_IT',
  * 'nb_NO', 'pt_PT', 'es_ES', 'sv_SE'.
  * Default value : [scayt_sLang='en_US']
  *
- * @property {String} scayt_uiTabs - Sets the visibility of particular tabs in the SCAYT dialog window and toolbar
- * button. This setting must contain a '1' (enabled) or '0'
+ * @property {String} scayt_uiTabs - The parameter customizes the SCAYT dialog and SCAYT toolbar menu to show particular tabs/items.
+ * This setting must contain a '1' (enabled) or '0'
  * (disabled) value for each of the following entries, in this precise order,
  * separated by a comma (','): 'Options', 'Languages', and 'Dictionary'.
  * Default value : [scayt_uiTabs='1,1,1']
  *
- * @property {String} scayt_serviceProtocol - Allows to specify protocol for WSC service (ssrv.cgi) full path.
+ * @property {String} scayt_serviceProtocol - The parameter allows to specify protocol for WSC service (ssrv.cgi) full path.
  * Default value : [scayt_serviceProtocol='http'].
  * Old option name : scayt_service_protocol
  *
- * @property {String} scayt_serviceHost - Allows to specify host for WSC service (ssrv.cgi) full path.
+ * @property {String} scayt_serviceHost - The parameter allows to specify host for WSC service (ssrv.cgi) full path.
  * Default value : [scayt_serviceHost='svc.webspellchecker.net'].
  * Old option name : scayt_service_host
  *
- * @property {String} scayt_servicePort - Allows to specify default port for WSC service (ssrv.cgi) full path.
+ * @property {String} scayt_servicePort - The parameter allows to specify default port for WSC service (ssrv.cgi) full path.
  * Default value : [scayt_servicePort='80'].
  * Old option name : scayt_service_port
  *
- * @property {String} scayt_servicePath - Allows to specify path for WSC service (ssrv.cgi) full path.
+ * @property {String} scayt_servicePath - The parameter allows to specify path for WSC service (ssrv.cgi) full path.
  * Default value : [scayt_servicePath='spellcheck31/script/ssrv.cgi'].
  * Old option name : scayt_service_path
  *
- * @property {String} scayt_srcUrl - Sets the URL to SCAYT core. Required to switch to the licensed version of SCAYT application.
+ * @property {String} scayt_srcUrl - The parameter sets the URL to SCAYT core. Required to switch to the licensed version of SCAYT application.
  * Further details available at [http://wiki.webspellchecker.net/doku.php?id=migration:hosredfreetolicensedck](http://wiki.webspellchecker.net/doku.php?id=migration:hosredfreetolicensedck)
  * Default value : [scayt_srcUrl = '//svc.webspellchecker.net/spellcheck31/lf/scayt3/ckscayt/ckscayt.js']
  *
- * @property {String} scayt_customDictionaryIds - Links SCAYT to custom dictionaries. This is a string containing dictionary IDs
+ * @property {String} scayt_customDictionaryIds - The parameter links SCAYT to custom dictionaries. This is a string containing dictionary IDs
  * separared by commas (',').
  * Further details at [http://wiki.webspellchecker.net/doku.php?id=installationandconfiguration:customdictionaries:licensed](http://wiki.webspellchecker.net/doku.php?id=installationandconfiguration:customdictionaries:licensed).
  * Default value : [scayt_customDictionaryIds='']
  *
- * @property {String} scayt_userDictionaryName - Makes it possible to activate a custom dictionary in SCAYT. The user
+ * @property {String} scayt_userDictionaryName - The parameter activates a User Dictionary in SCAYT. The user
  * dictionary name must be used.
  * Default value : [scayt_userDictionaryName=null]
  *
- * @property {String} scayt_contextMenuItemsOrder - Defines the order SCAYT context menu items by groups.
+ * @property {String} scayt_contextMenuItemsOrder - The parameter defines the order SCAYT context menu items by groups.
  * This must be a string with one or more of the following
  * words separated by a pipe character ('|'):
  * 'suggest' &ndash; main suggestion word list;
@@ -90,8 +90,8 @@
  * If set to false, provides better performance on undo\redo actions
  * Default value : [scayt_handleUndoRedo=true]
  *
- * @property {String} scayt_elementsToIgnore - Specifies the names of tags that will be skipped when spellchecking. This is a string containing tag names
- * separared by commas (','). Please note that 'style' tag would be added to specified tags list.
+ * @property {String} scayt_elementsToIgnore - Specifies the names of tags that will be skipped while spell checking. It is a string containing tag names separared by commas (',').
+ * Please note that 'style' tag would be added to specified tags list.
  * Default value : [scayt_elementsToIgnore='style']
  *
  * @example
